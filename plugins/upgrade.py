5a005a6938dd23171c475e787aabeb7acb21cddc
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-05-06 08:37:01 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -20,15 +20,15 @@ Daily Upload limit: 100GB
 Price: 150₹ /🌎 2.16$ per Month
 
 Payment Details:
-<b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a></b>
+<b>➜ <a href="https://envs.sh/kpL.jpg">Click Here To Scan</a></b>
 
 💵 𝗔𝗡𝗬 𝗖𝗢𝗨𝗡𝗧𝗥𝗬 𝗔𝗟𝗟 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘. 
 যদি বিকাশ বা 𝗤𝗥 কোড ছাড়া অথবা অন্য কিছু মাধ্যমে পেমেন্ট করতে চাইলে অথবা আরো কিছু জানার থাকলে 
-𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Prime_Admin_Support_ProBot">𝐌𝐑.𝐏𝐑𝐈𝐌𝐄</a> \n\n
+𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Indian_MV_Admin_Bot">🇮🇳 𝐀𝐝𝐦𝐢𝐧 🧑🏻‍💻</a> \n\n
 👇Send Payment Receipt 🧾 Screenshot"""
     
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Click Here To Send Payment Receipt 🧾", url="https://t.me/Prime_Admin_Support_BOT")],
+        [InlineKeyboardButton("🦋 Click Here To Send Payment Receipt 🧾", url="https://t.me/Indian_MV_Admin_Bot")],
         [InlineKeyboardButton("✖️ Cancel ✖️", callback_data="cancel")]
     ])
     
@@ -58,15 +58,15 @@ Daily Upload limit: 100GB
 Price: 150₹ /🌎 2.16$ per Month
 
 Payment Details:
-<b>➜ <a href="https://envs.sh/AR9.jpg">Click Here To Scan</a></b>
+<b>➜ <a href="https://envs.sh/kpL.jpg">Click Here To Scan</a></b>
 
 💵 𝗔𝗡𝗬 𝗖𝗢𝗨𝗡𝗧𝗥𝗬 𝗔𝗟𝗟 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗠𝗘𝗧𝗛𝗢𝗗 𝗔𝗩𝗔𝗜𝗟𝗔𝗕𝗟𝗘. 
 যদি বিকাশ বা 𝗤𝗥 কোড ছাড়া অথবা অন্য কিছু মাধ্যমে পেমেন্ট করতে চাইলে অথবা আরো কিছু জানার থাকলে 
-𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Prime_Admin_Support_ProBot">𝐌𝐑.𝐏𝐑𝐈𝐌𝐄</a> \n\n
+𝗖𝗢𝗡𝗡𝗘𝗖𝗧 𝗔𝗗𝗠𝗜𝗡 ➠ <a href="https://t.me/Indian_MV_Admin_Bot">🇮🇳 𝐀𝐝𝐦𝐢𝐧 🧑🏻‍💻</a> \n\n
 👇(Admin)👇Send Payment Receipt 🧾 Screenshot"""
 
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin", url="https://t.me/Prime_Admin_Support_BOT"),
+        [InlineKeyboardButton("🦋 Admin", url="https://t.me/Indian_MV_Admin_Bot"),
          InlineKeyboardButton("✖️ Cancel", callback_data="cancel")]
     ])
     
